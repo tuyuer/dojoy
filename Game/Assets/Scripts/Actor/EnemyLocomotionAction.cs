a2029997439bb637c1662e6852089dde98a61040
zhh <wowo5050@126.com>
2021-01-31 21:35:38 +0800

    ^ Arrange AI Logic

---
+++ Game/Assets/Scripts/Actor/EnemyLocomotionAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLocomotionAction : ActorAction
+{
+    public EnemyLocomotionAction()
+    {
+        actionType = actor_state.actor_state_locomotion;
+    }
+
+    public override void Update(float deltaTime)
+    {
+        float forwardSpeed = Mathf.Min(blackboard.actorSpeed.magnitude, 2.0f);
+        if (forwardSpeed < 0.1f)
+        {
+            forwardSpeed = 0;
+        }
+        //animator
+        animator.SetFloat(AnimatorParameter.ForwardSpeed, forwardSpeed);
+    }
+}
--- Game/Assets/Scripts/Enemy/EnemyBrain.cs
+++ Game/Assets/Scripts/Enemy/EnemyBrain.cs
@@ -1,17 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBrain : Brain
 {
+    NavMeshAgent navMeshAgent;
     // Start is called before the first frame update
     void Awake()
     {
-        base.Awake();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        blackboard = GetComponent<ActorBlackboard>();
+        if (blackboard.swordSocket != null)
+        {
+            blackboard.swordSocket.gameObject.SetActive(true);
+        }
+
+        actionList.Add(actor_state.actor_state_locomotion, new EnemyLocomotionAction());
+        actionList.Add(actor_state.actor_state_climb, new ClimbAction());
+        actionList.Add(actor_state.actor_state_jump, new JumpAction());
+        actionList.Add(actor_state.actor_state_land, new LandAction());
+        actionList.Add(actor_state.actor_state_vault, new VaultAction());
+        actionList.Add(actor_state.actor_state_dodge, new DodgeAction());
+        actionList.Add(actor_state.actor_state_punch, new PunchAction());
+        actionList.Add(actor_state.actor_state_sword_attack, new SwordAttackAction());
+
+        foreach (KeyValuePair<actor_state, ActorAction> kv in actionList)
+        {
+            ActorAction actorAction = kv.Value;
+            actorAction.AttachBlackboard(blackboard);
+        }
     }
 
     void Update()
     {
-        base.Update();
+        //input direction
+        blackboard.moveDir = transform.forward;
+        Vector3 actorSpeed = navMeshAgent.velocity;
+
+        //apply gravity
+        actorSpeed.y -= GlobalDef.WORLD_GRAVITY * Time.deltaTime;
+
+        //set actorSpeed to Blackboard
+        blackboard.actorSpeed = actorSpeed;
+
+        //update active action in actionList
+        foreach (KeyValuePair<actor_state, ActorAction> kv in actionList)
+        {
+            ActorAction actorAction = kv.Value;
+            bool isActionEnabled = actorAction.ActionType == blackboard.actorState;
+            actorAction.setEnabled(isActionEnabled);
+            if (actorAction.IsEnabled)
+            {
+                actorAction.Update(Time.deltaTime);
+            }
+        }
     }
 }
