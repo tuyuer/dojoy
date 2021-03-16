61760af87d94b8031daef1e115c72910800325eb
tuyuer <wowo5050@126.com>
2021-03-16 12:03:46 +0800

    *support camera rotate

---
+++ Game/Assets/Scripts/Logic/GameObject/TransformObject.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransformObject : MonoBehaviour
+{
+    //旋转最大角度
+    public int yMinLimit = -20;
+    public int yMaxLimit = 80;
+    //旋转速度
+    public float xSpeed = 250.0f;
+    public float ySpeed = 120.0f;
+    //旋转角度
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input.GetAxis("MouseX")获取鼠标移动的X轴的距离
+        x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+        y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+        y = ClampAngle(y, yMinLimit, yMaxLimit);
+        //欧拉角转化为四元数
+        Quaternion rotation = Quaternion.Euler(y, x, 0);
+        transform.rotation = rotation;
+    }
+
+    static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Mathf.Clamp(angle, min, max);
+   }
+}
