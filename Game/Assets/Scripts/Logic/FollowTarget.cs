13130a6a8ef69157583a0f059f42a573bf411ae0
tuyuer <wowo5050@126.com>
2021-03-16 11:20:39 +0800

    setup camera

---
+++ Game/Assets/Scripts/Logic/FollowTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform followTarget = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (followTarget)
+        {
+            transform.position = followTarget.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (followTarget)
+        {
+            transform.position = followTarget.position;
+        }
+    }
+}
